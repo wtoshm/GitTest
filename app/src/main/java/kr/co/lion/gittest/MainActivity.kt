c359e97784df72f259b7d101c79d05230c60692d
wtoshm <wtoshm@gmail.com>
2024-02-08 13:32:59 +0900

    Test Commit 6

--- app/src/main/java/kr/co/lion/gittest/MainActivity.kt
+++ app/src/main/java/kr/co/lion/gittest/MainActivity.kt
@@ -9,8 +9,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val a1 = 10
-        val a2 = 20
-        val a3 = 30
-        val a4 = 40
+
     }
 }
